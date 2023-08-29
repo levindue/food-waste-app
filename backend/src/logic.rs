f93ebc148bd9f8c7a7a5d01517217d0b4cec98fc
levindue <levin@duesterhus.net>
2023-08-29 09:10:53 +0200

    test

--- backend/src/logic.rs
+++ backend/src/logic.rs
@@ -2,6 +2,8 @@ use serde::{Deserialize, Serialize};
 use std::fs::{File, OpenOptions};
 use std::io::Read;
 
+// hallo jonas
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Person {
     pub id: u32,
