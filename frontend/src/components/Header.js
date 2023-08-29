08fa95b60c4e0055d67e8fee65d3cf2ce10098ee
jonaso <jonas-osewold@web.de>
2023-08-29 10:54:41 +0200

    feat: added navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,13 @@
 function Header(){
     return(
         <div className="App-header">
-             
+            <div className="App-login">
+                <button id="login">Anmelden</button>
+                <button id="register">Registrieren</button>
+            </div>
         </div>
     )
 }
 
 export default Header;
+
