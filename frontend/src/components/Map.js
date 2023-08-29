7ef906aebcc92ba4783ffb032a5c153259a908bd
levindue <levin@duesterhus.net>
2023-08-29 10:25:54 +0200

    feat: leaflet map

---
+++ frontend/src/components/Map.js
@@ -0,0 +1,23 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './Map.css';
+
+function Map() {
+  const position = [51.505, -0.09]
+  return (
+    <div className="Map">
+      <MapContainer center={position} zoom={12} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            Test popup
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  )
+}
+
+export default Map;
