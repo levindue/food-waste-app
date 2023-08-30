f6623e96e8806d0449cf97c603d730c0e04afea8
jonaso <jonas-osewold@web.de>
2023-08-30 11:52:32 +0200

    feat: added list page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import './styles/App.css';
 import Test from './pages/Test';
+import List from './pages/List';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/list' element={<List />} />
         <Route path="/test" element={<Test />} />
       </Routes>
     </div>
---
+++ frontend/src/pages/List.js
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+
+function List() {
+    return(
+        <div>
+            <Header />
+            <ul>
+<li>Coffee</li>
+<li>Tea</li>
+<li>Milk</li>
+</ul>
+        </div>
+    )
+}
+
+export default List;
