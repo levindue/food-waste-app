bbb2d55ff75a44250ce0d9d04541b885640ef3ee
levindue <levin@duesterhus.net>
2023-08-31 08:44:41 +0200

    fix: make map better ig

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -16,7 +16,7 @@ function Map() {
     <div className="Map">
       <MapContainer center={position} zoom={15} scrollWheelZoom={false}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={position} icon={myIcon}>
